f548eb82abc5bbef4a5c603e7cd70d4123db5830
Shiro <1796475983@qq.com>
2025-03-31 14:44:58 +0800

    项目初始化

---
+++ smart-campus-backend/src/test/java/com/shiro/campus/CampusIntegratedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shiro.campus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampusIntegratedApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
